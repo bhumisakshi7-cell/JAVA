253dd1704b8681d62468d3a19d77b15a9b6708f1
bhumisakshi7-cell <bhumisakshi7@gmail.com>
2026-08-02 20:16:58 +0530

    Update SumOfDigits.java

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -8,14 +8,14 @@ public class SumOfDigits {
 		  
 	        System.out.println("Enter an integer between 0 and 1000: ");
 	        int number = sc.nextInt();
-	        int original = number;                                            // Store original number
-	        int sum = 0;                                                      // Variable to store sum
-	        while (number > 0) {                                               // Find sum of digits
-	          int digit = number % 10;                                        // Get last digit
-	            sum = sum + digit;                                           // Add digit to sum
-	            number = number / 10;                                        // Remove last digit
+	        int original = number;                                            
+	        int sum = 0;                                                      
+	        while (number > 0) {                                               
+	          int digit = number % 10;                                       
+	            sum = sum + digit;                                           
+	            number = number / 10;                                        
 	        }
-  System.out.println("The sum of the digits of " + original + " is " + sum);     // Output
+  System.out.println("The sum of the digits of " + original + " is " + sum);     
 
 	        sc.close();
 
