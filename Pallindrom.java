7483acdfae235e3cb88eb0df2eebf9369090b847
bhumisakshi7-cell <bhumisakshi7@gmail.com>
2026-08-02 21:43:48 +0530

    Add files via upload

---
+++ Pallindrom.java
@@ -0,0 +1,31 @@
+package com.studycse;
+import java.util.Scanner;
+public class Pallindrom {
+
+//	public Pallindrom() {
+		 public static void main(String[] args) {
+		        Scanner sc = new Scanner(System.in);
+
+		        System.out.println("Enter a string: ");
+		        String str = sc.nextLine();
+
+		        String reversed = "";
+
+		        // Reverse the string
+		        for (int i = str.length() - 1; i >= 0; i--) {
+		            reversed += str.charAt(i);
+		        }
+
+		        // Check if original and reversed strings are equal
+		        if (str.equals(reversed)) {
+		            System.out.println("The string is a palindrome.");
+		        } else {
+		            System.out.println("The string is not a palindrome.");
+		        }
+
+		        sc.close();
+		    }
+
+	}
+
+//}
